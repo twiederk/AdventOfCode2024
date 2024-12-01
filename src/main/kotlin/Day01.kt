a2973415689fe95c1fbc0b619fca7928255afab8
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-01 06:25:47 +0100

    Implemented load data

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,4 +1,16 @@
+import java.nio.file.Path
+import kotlin.io.path.name
+
 class Day01 {
 
-    fun isResult(): Boolean = true
+    fun loadData(path: Path): Pair<List<Int>, List<Int>> {
+        val first = mutableListOf<Int>()
+        val second = mutableListOf<Int>()
+        Resources.resourceAsListOfString(path.name).map {
+            val values = it.split("   ")
+            first.add(values[0].toInt())
+            second.add(values[1].toInt())
+        }
+        return Pair(first, second)
+    }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,16 +1,21 @@
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
 
 class Day01Test {
 
     @Test
-    fun should_just_run() {
+    fun should_load_data_in_two_lists() {
+
+        // arrange
+        val path = Path("src", "test", "resources", "Day01_TestData.txt")
 
         // act
-        val result = Day01().isResult()
+        val result = Day01().loadData(path)
 
         // assert
-        Assertions.assertThat(result).isTrue()
+        assertThat(result.first).containsExactly(3, 4, 2, 1, 3, 3)
+        assertThat(result.second).containsExactly(4, 3, 5, 3, 9, 3)
     }
 
 }
