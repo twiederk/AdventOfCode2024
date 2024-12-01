4fdf8416110c09743c09b2711bcff588507646ae
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-01 06:34:30 +0100

    Implemented solution part1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,5 +1,6 @@
 import java.nio.file.Path
 import kotlin.io.path.name
+import kotlin.math.abs
 
 class Day01 {
 
@@ -13,4 +14,26 @@ class Day01 {
         }
         return Pair(first, second)
     }
+
+    fun sortData(first: List<Int>, second: List<Int>): Pair<List<Int>, List<Int>> {
+        return Pair(first.sorted(), second.sorted())
+    }
+
+    fun calcDistance(start: Int, end: Int): Int {
+        return abs(end - start)
+    }
+
+    fun sumUpDistance(first: List<Int>, second: List<Int>): Int {
+        var sum = 0
+        for (i in first.indices) {
+            sum += calcDistance(first[i], second[i])
+        }
+        return sum
+    }
+
+    fun part1(first: List<Int>, second: List<Int>): Int {
+        val sorted = sortData(first, second)
+        return sumUpDistance(sorted.first, sorted.second)
+    }
+
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -18,4 +18,65 @@ class Day01Test {
         assertThat(result.second).containsExactly(4, 3, 5, 3, 9, 3)
     }
 
+    @Test
+    fun should_sort_both_lists() {
+        // arrange
+        val first = listOf(3, 4, 2, 1, 3, 3)
+        val second = listOf(4, 3, 5, 3, 9, 3)
+
+        // act
+        val result = Day01().sortData(first, second)
+
+        // assert
+        assertThat(result.first).containsExactly(1, 2, 3, 3, 3, 4)
+        assertThat(result.second).containsExactly(3, 3, 3, 4, 5, 9)
+    }
+
+    @Test
+    fun should_calc_distance_between_both_values_first_value_lower() {
+        // arrange
+
+        // act
+        val result = Day01().calcDistance(1, 3)
+
+        // assert
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun should_calc_distance_between_both_values_second_value_lower() {
+
+        // act
+        val result = Day01().calcDistance(3, 1)
+
+        // assert
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun should_sum_up_total_distance() {
+        // arrange
+        val first = listOf(1, 2, 3, 3, 3, 4)
+        val second = listOf(3, 3, 3, 4, 5, 9)
+
+        // act
+        val result = Day01().sumUpDistance(first, second)
+
+        // assert
+        assertThat(result).isEqualTo(11)
+    }
+
+    @Test
+    fun should_solve_part1() {
+        // arrange
+        val first = listOf(3, 4, 2, 1, 3, 3)
+        val second = listOf(4, 3, 5, 3, 9, 3)
+
+        // act
+        val result = Day01().part1(first, second)
+
+        // assert
+        assertThat(result).isEqualTo(11)
+
+    }
 }
