4db531c2a54df8beb215073971c222e3bd147042
IN57KC <torsten.wiederkehr@ing.com>
2024-12-04 14:59:21 +0100

    Solved part 1 of Day04

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,4 +1,5 @@
 import java.nio.file.Path
+import kotlin.io.path.Path
 import kotlin.io.path.name
 
 class Day04 {
@@ -9,37 +10,26 @@ class Day04 {
         'A' to 'S'
     )
 
+    private val directions = listOf(
+        Point2D.NORTH,
+        Point2D.NORTH_EAST,
+        Point2D.EAST,
+        Point2D.SOUTH_EAST,
+        Point2D.SOUTH,
+        Point2D.SOUTH_WEST,
+        Point2D.WEST,
+        Point2D.NORTH_WEST
+    )
+
 
     fun loadData(path: Path): List<String> {
         return Resources.resourceAsListOfString(path.name)
     }
 
-    fun bfs(wordPuzzle: List<String>): Int {
-        /*
-        val queue = LinkedList<Work>()
-        queue.add(Work(startingPosition, 0))
-        val finalSteps = mutableListOf<Work>()
-
-        while (queue.isNotEmpty()) {
-            val current = queue.remove()
-            if (current.steps == maxSteps) {
-                finalSteps.add(current)
-                continue
-            }
-            current.position.cardinalNeighbors(mapOfGarden)
-                .filter { !isRock(mapOfGarden, it) }
-                .filter { it !in queue.map { queueElement -> queueElement.position } }
-                .forEach {
-                    queue.add(Work(it, current.steps + 1))
-                }
-        }
-        return finalSteps.map { it.position }
-         */
-
-        return 0
-    }
-
     fun letter(point: Point2D, wordPuzzle: List<String>): Char {
+        if (point.x < 0 || point.y < 0 || point.y >= wordPuzzle.size || point.x >= wordPuzzle[point.y].length) {
+            return '-'
+        }
         return wordPuzzle[point.y][point.x]
     }
 
@@ -48,4 +38,47 @@ class Day04 {
         val nextLetter = letterMap[letter(point, wordPuzzle)]
         return allNeighbors.filter { letter(it, wordPuzzle) == nextLetter }
     }
+
+    fun startingPoints(wordPuzzle: List<String>, letter: Char): List<Point2D> {
+        val startingPoints = mutableListOf<Point2D>()
+        for (y in wordPuzzle.indices) {
+            for (x in wordPuzzle[y].indices) {
+                if (letter(Point2D(x, y), wordPuzzle) == letter) {
+                    startingPoints.add(Point2D(x, y))
+                }
+            }
+        }
+        return startingPoints
+    }
+
+    fun word(point: Point2D, direction: Point2D, worldPuzzle: List<String>): Boolean {
+        return (letter(point + direction, worldPuzzle) == 'M'
+                && letter(point + direction + direction, worldPuzzle) == 'A'
+                && letter(point + direction + direction + direction, worldPuzzle) == 'S')
+    }
+
+    fun numberOfWords(point: Point2D, wordPuzzle: List<String>): Int {
+        var numberOfWords = 0
+        for (direction in directions) {
+            if (word(point, direction, wordPuzzle)) {
+                numberOfWords++
+            }
+        }
+        return numberOfWords
+    }
+
+    fun solvePart1(wordPuzzle: List<String>): Int {
+        var count = 0
+        for (point in startingPoints(wordPuzzle, 'X')) {
+            count += numberOfWords(point, wordPuzzle)
+        }
+        return count
+    }
+
+}
+
+fun main() {
+    val day04 = Day04()
+    val wordPuzzle = Day03().loadData(Path("src", "main", "resources", "Day04_InputData.txt"))
+    println("Part 1: ${day04.solvePart1(wordPuzzle)}")
 }
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -4,6 +4,14 @@ import kotlin.io.path.Path
 
 class Day04Test {
 
+    private val wordPuzzle = listOf(
+        "..X...",
+        ".SAMX.",
+        ".A..A.",
+        "XMAS.S",
+        ".X....",
+    )
+
     @Test
     fun should_load_data() {
 
@@ -18,33 +26,7 @@ class Day04Test {
     }
 
     @Test
-    fun should_perform_bfs() {
-        // arrange
-        val wordPuzzle = listOf(
-            "..X...",
-            ".SAMX.",
-            ".A..A.",
-            "XMAS.S",
-            ".X....",
-        )
-
-        // act
-        val result = Day04().bfs(wordPuzzle)
-
-        // assert
-        assertThat(result).isEqualTo(4)
-    }
-
-    @Test
     fun should_return_letter_at_position() {
-        // arrange
-        val wordPuzzle = listOf(
-            "..X...",
-            ".SAMX.",
-            ".A..A.",
-            "XMAS.S",
-            ".X....",
-        )
 
         // act
         val result = Day04().letter(Point2D(2, 0), wordPuzzle)
@@ -54,15 +36,18 @@ class Day04Test {
     }
 
     @Test
+    fun should_return_minus_sign_when_letter_not_on_grid() {
+
+        // act
+        var result = Day04().letter(Point2D(-1, -1), wordPuzzle)
+
+        // assert
+        assertThat(result).isEqualTo('-')
+
+    }
+
+    @Test
     fun should_return_list_of_neighbors_at_2_0() {
-        // arrange
-        val wordPuzzle = listOf(
-            "..X...",
-            ".SAMX.",
-            ".A..A.",
-            "XMAS.S",
-            ".X....",
-        )
 
         // act
         val neighbors = Day04().neighbours(Point2D(2, 0), wordPuzzle)
@@ -71,7 +56,82 @@ class Day04Test {
         assertThat(neighbors).containsExactly(Point2D(3, 1))
     }
 
-    // test getNeighbours(x, y)
-    // test get all starting points
+    @Test
+    fun should_return_starting_points() {
+
+        // act
+        val startingPoints = Day04().startingPoints(wordPuzzle, 'X')
+
+        // assert
+        assertThat(startingPoints).hasSize(4)
+    }
+
+    @Test
+    fun should_return_true_when_horizontal_left_to_right_word() {
+
+        // act
+        val result = Day04().word(Point2D(0, 3), Point2D.EAST, wordPuzzle)
+
+        // assert
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun should_return_true_when_starting_points_is_0_0() {
+
+        // act
+        val result = Day04().word(Point2D(0, 0), Point2D.EAST, wordPuzzle)
+
+        // assert
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun should_check_all_directions_of_starting_point() {
+
+        // act
+        val result = Day04().numberOfWords(Point2D(2, 0), wordPuzzle)
+
+        // assert
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun should_solve_simple_puzzle() {
+
+        // act
+        val result = Day04().solvePart1(wordPuzzle)
+
+        // assert
+        assertThat(result).isEqualTo(4)
+
+    }
+
+    @Test
+    fun should_solve_test_puzzle() {
+        // arrange
+        val testPuzzle = listOf(
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        )
+
+        // act
+        val result = Day04().solvePart1(testPuzzle)
+
+        // assert
+        assertThat(result).isEqualTo(18)
+    }
 
 }
+
+
+
+
