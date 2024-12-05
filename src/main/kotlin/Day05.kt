9cd1d244af97cfb47eee99f58abdc8cda6ac9241
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-05 06:24:00 +0100

    Started implementation of Day05

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,2 +1,8 @@
 class Day05 {
+
+    fun validatePageOrder(page: Int, rules: Map<Int, List<Int>>, update: List<Int>): Boolean {
+        val pageRules = rules[page] ?: return false
+        return pageRules.containsAll(update)
+    }
+
 }
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -1,2 +1,20 @@
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class Day05Test {
+
+    @Test
+    fun should_check_if_75_is_in_the_right_place() {
+        // arrange
+        val rules = mapOf(
+            75 to listOf(29, 53, 47, 61, 13)
+        )
+        val update = listOf(47, 61, 53, 29)
+
+        // act
+        val result = Day05().validatePageOrder(75, rules, update)
+
+        // assert
+        assertThat(result).isTrue()
+    }
 }
