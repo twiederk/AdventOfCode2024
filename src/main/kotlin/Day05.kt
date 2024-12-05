81649589fc5bbd747a277f49e0be3e49d6d4a623
IN57KC <torsten.wiederkehr@ing.com>
2024-12-05 11:40:31 +0100

    Solved Day05

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,12 +1,8 @@
 import java.nio.file.Path
+import kotlin.io.path.Path
 import kotlin.io.path.name
 
 class Day05 {
-
-    fun validatePageOrder(page: Int, rules: Map<Int, List<Int>>, update: List<Int>): Boolean {
-        val pageRules = rules[page] ?: return false
-        return pageRules.containsAll(update)
-    }
 
     fun loadRules(path: Path): Pair<Map<Int, MutableList<Int>>, List<List<Int>>> {
         val data = Resources.resourceAsListOfString(path.name)
@@ -32,15 +28,83 @@ class Day05 {
     }
 
     fun validateUpdate(update: List<Int>, rules: Map<Int, List<Int>>): Boolean {
-        var currentUpdate = update
-        while (currentUpdate.isNotEmpty()) {
-            val page = currentUpdate[0]
-            currentUpdate = currentUpdate.drop(1)
-            if (!validatePageOrder(page, rules, currentUpdate)) {
+        var remainingUpdate = update
+        while (remainingUpdate.size > 1) {
+            val currentPage = remainingUpdate[0]
+            remainingUpdate = remainingUpdate.drop(1)
+            if (!validateNextStep(currentPage, remainingUpdate[0], rules)) {
                 return false
             }
         }
         return true
     }
 
+    fun validateUpdates(updates: List<List<Int>>, rules: Map<Int, List<Int>>): List<List<Int>> {
+        return updates.filter { validateUpdate(it, rules) }
+    }
+
+    fun invalidateUpdates(updates: List<List<Int>>, rules: Map<Int, List<Int>>): List<List<Int>> {
+        return updates.filter { !validateUpdate(it, rules) }
+    }
+
+
+    fun validateNextStep(currentPage: Int, nextPage: Int, rules: Map<Int, List<Int>>): Boolean {
+        val pageRules = rules[currentPage] ?: return false
+        return pageRules.contains(nextPage)
+    }
+
+    fun calculateFinalScore(validUpdates: List<List<Int>>): Int {
+        val middleElements = validUpdates.map { it[it.size / 2] }
+        return middleElements.sum()
+    }
+
+    fun solvePart1(rules: Map<Int, List<Int>>, updates: List<List<Int>>): Int {
+        val validUpdates = validateUpdates(updates, rules)
+        return calculateFinalScore(validUpdates)
+    }
+
+    fun findBrokenIndex(update: List<Int>, rules: Map<Int, List<Int>>): Int {
+        update.windowed(2, 1).forEachIndexed { index, pair ->
+            if (!validateNextStep(pair[0], pair[1], rules)) {
+                return index
+            }
+        }
+        return -1
+    }
+
+    fun switchElements(update: List<Int>, index: Int): List<Int> {
+        val first = update[index]
+        val second = update[index + 1]
+        val newUpdate = update.toMutableList()
+        newUpdate[index] = second
+        newUpdate[index + 1] = first
+        return newUpdate
+    }
+
+    fun correctBrokenUpdate(update: List<Int>, rules: Map<Int, List<Int>>): List<Int> {
+        var index = findBrokenIndex(update, rules)
+        var newUpdate = update
+        while (index != -1) {
+            newUpdate = switchElements(newUpdate, index)
+            if (validateUpdate(newUpdate, rules)) {
+                return newUpdate
+            }
+            index = findBrokenIndex(newUpdate, rules)
+        }
+        return throw IllegalArgumentException("No valid update found")
+    }
+
+    fun solvePart2(rules: Map<Int, List<Int>>, updates: List<List<Int>>): Int {
+        val invalidUpdates = invalidateUpdates(updates, rules)
+        val validUpdates = invalidUpdates.map { correctBrokenUpdate(it, rules) }
+        return calculateFinalScore(validUpdates)
+    }
+
+}
+
+fun main() {
+    val day05 = Day05()
+    val (rules, updates) = day05.loadRules(Path("src", "main", "resources", "Day05_InputData.txt"))
+    println("Part 1: ${day05.solvePart1(rules, updates)}")
+    println("Part 2: ${day05.solvePart2(rules, updates)}")
 }
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -13,6 +13,15 @@ class Day05Test {
         53 to listOf(29, 13)
     )
 
+    private val exampleUpdates = listOf(
+        listOf(75, 47, 61, 53, 29),
+        listOf(97, 61, 53, 29, 13),
+        listOf(75, 29, 13),
+        listOf(75, 97, 47, 61, 53),
+        listOf(61, 13, 29),
+        listOf(97, 13, 75, 29, 47),
+    )
+
     @Test
     fun should_load_rules() {
         // arrange
@@ -28,24 +37,34 @@ class Day05Test {
     }
 
     @Test
-    fun should_check_if_75_is_in_the_right_place() {
+    fun should_validate_next_number() {
         // arrange
-        val rules = mapOf(
-            75 to listOf(29, 53, 47, 61, 13)
-        )
-        val update = listOf(47, 61, 53, 29)
+        val update = listOf(75, 47, 61, 53, 29)
 
         // act
-        val result = Day05().validatePageOrder(75, rules, update)
+        val result = Day05().validateNextStep(75, 47, exampleRules)
+
+        // assert
+        assertThat(result).isTrue()
+
+    }
+
+    @Test
+    fun should_validate_update_75_29_53_47_61_13() {
+        // arrange
+        val update = listOf(75, 47, 61, 53, 29)
+
+        // act
+        val result = Day05().validateUpdate(update, exampleRules)
 
         // assert
         assertThat(result).isTrue()
     }
 
     @Test
-    fun should_validate_update_to_true() {
+    fun should_validate_update_75_29_13() {
         // arrange
-        val update = listOf(75, 47, 61, 53, 29)
+        val update = listOf(75, 29, 13)
 
         // act
         val result = Day05().validateUpdate(update, exampleRules)
@@ -64,6 +83,91 @@ class Day05Test {
 
         // assert
         assertThat(result).isFalse()
+    }
+
+    @Test
+    fun should_validate_example_data() {
+
+        // act
+        val validUpdates = Day05().validateUpdates(exampleUpdates, exampleRules)
+
+        // assert
+        assertThat(validUpdates).containsExactly(
+            listOf(75, 47, 61, 53, 29),
+            listOf(97, 61, 53, 29, 13),
+            listOf(75, 29, 13),
+        )
+    }
+
+    @Test
+    fun should_calculate_final_score() {
+        // arrange
+        val validUpdates = listOf(
+            listOf(75, 47, 61, 53, 29),
+            listOf(97, 61, 53, 29, 13),
+            listOf(75, 29, 13),
+        )
+
+        // act
+        val result = Day05().calculateFinalScore(validUpdates)
+
+        // assert
+        assertThat(result).isEqualTo(143)
+    }
+
+    @Test
+    fun should_solve_part1() {
+
+        // act
+        val result = Day05().solvePart1(exampleRules, exampleUpdates)
+
+        // assert
+        assertThat(result).isEqualTo(143)
+    }
+
+    @Test
+    fun should_return_index_of_broken_path() {
+        // arrange
+        val update = listOf(97, 13, 75, 29, 47)
+
+        // act
+        val index = Day05().findBrokenIndex(update, exampleRules)
+
+        // assert
+        assertThat(index).isEqualTo(1)
+    }
+
+    @Test
+    fun should_switch_elements_at_given_index() {
+        // arrange
+        val update = listOf(97, 13, 75, 29, 47)
+
+        // act
+        val result = Day05().switchElements(update, 1)
+
+        // assert
+        assertThat(result).containsExactly(97, 75, 13, 29, 47)
+    }
+
+    @Test
+    fun should_correct_broken_update() {
+        // arrange
+        val update = listOf(97, 13, 75, 29, 47)
+
+        // act
+        val result = Day05().correctBrokenUpdate(update, exampleRules)
+
+        // assert
+        assertThat(result).containsExactly(97, 75, 47, 29, 13)
+    }
+
+    @Test
+    fun should_solve_part2() {
+        // act
+        val result = Day05().solvePart2(exampleRules, exampleUpdates)
+
+        // assert
+        assertThat(result).isEqualTo(123)
     }
 
 }
