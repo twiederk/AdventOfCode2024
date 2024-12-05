3c96c76cd514137e51967f816ddd1d23d1acfd41
IN57KC <torsten.wiederkehr@ing.com>
2024-12-05 08:45:58 +0100

    Implemented import of rules and updates.

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,8 +1,34 @@
+import java.nio.file.Path
+import kotlin.io.path.name
+
 class Day05 {
 
     fun validatePageOrder(page: Int, rules: Map<Int, List<Int>>, update: List<Int>): Boolean {
         val pageRules = rules[page] ?: return false
         return pageRules.containsAll(update)
+    }
+
+    fun loadRules(path: Path): Pair<Map<Int, MutableList<Int>>, List<List<Int>>> {
+        val data = Resources.resourceAsListOfString(path.name)
+        val emptyLineIndex = data.indexOfFirst { it.isEmpty() }
+
+        val rules = rules(data, emptyLineIndex)
+        val updates: List<List<Int>> = data.subList(emptyLineIndex + 1, data.size)
+            .map { input -> input.split(",").map { it.toInt() } }
+        return Pair(rules, updates)
+
+    }
+
+    private fun rules(data: List<String>, emptyLine: Int): MutableMap<Int, MutableList<Int>> {
+        val rules = mutableMapOf<Int, MutableList<Int>>()
+        val ruleData = data.subList(0, emptyLine)
+        ruleData.forEach {
+            val (page1, page2) = it.split("|")
+            val pages = rules.getOrDefault(page1.toInt(), mutableListOf())
+            pages.add(page2.toInt())
+            rules.put(page1.toInt(), pages)
+        }
+        return rules
     }
 
 }
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -1,7 +1,21 @@
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
 
 class Day05Test {
+
+    @Test
+    fun should_load_rules() {
+        // arrange
+        val path = Path("src", "test", "resources", "Day05_TestData.txt")
+
+        // act
+        val (rules, updates) = Day05().loadRules(path)
+
+        // assert
+        assertThat(rules[75]).containsExactly(29, 53, 47, 61, 13)
+        assertThat(updates).hasSize(6)
+    }
 
     @Test
     fun should_check_if_75_is_in_the_right_place() {
