47643d21e948401c7dc34336de533d2ef08df07f
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-06 17:26:35 +0100

    Implemented base move and turn actions

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,2 +1,50 @@
+import java.nio.file.Path
+import kotlin.io.path.name
+
+
 class Day06 {
+
+
+    private val turns = mapOf(
+        Point2D.NORTH to Point2D.EAST,
+        Point2D.EAST to Point2D.SOUTH,
+        Point2D.SOUTH to Point2D.WEST,
+        Point2D.WEST to Point2D.NORTH,
+    )
+
+    fun loadData(path: Path): List<String> {
+        return Resources.resourceAsListOfString(path.name)
+    }
+
+    fun startPoint(grid: List<String>): Point2D {
+        for (y in grid.indices) {
+            for (x in grid[y].indices) {
+                if (grid[y][x] == '^') {
+                    return Point2D(x, y)
+                }
+            }
+        }
+        throw IllegalArgumentException("No start point found")
+    }
+
+    fun isBlocked(position: Point2D, grid: List<String>): Boolean {
+        return grid[position.y][position.x] == '#'
+    }
+
+    fun move(position: Point2D, direction: Point2D, grid: List<String>): Point2D {
+        val newPosition = position + direction
+        if (newPosition.x < 0 || newPosition.y < 0 || newPosition.y >= grid.size || newPosition.x >= grid[newPosition.y].length) {
+            return END_POINT
+        }
+        return newPosition
+    }
+
+    fun turn(direction: Point2D): Point2D {
+        return turns[direction] ?: throw IllegalArgumentException("Invalid direction")
+    }
+
+    companion object {
+        val END_POINT = Point2D(-1, -1)
+    }
+
 }
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -1,2 +1,92 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+
+
 class Day06Test {
+
+    val grid = listOf(
+        "....#.....",
+        ".........#",
+        "..........",
+        "..#.......",
+        ".......#..",
+        "..........",
+        ".#..^.....",
+        "........#.",
+        "#.........",
+        "......#...",
+    )
+
+    @Test
+    fun should_load_data() {
+
+        // act
+        val grid = Day06().loadData(Path("src", "test", "resources", "Day06_TestData.txt"))
+
+        // assert
+        assertThat(grid).hasSize(10)
+    }
+
+    @Test
+    fun should_find_start_position() {
+
+        // act
+        val startPoint = Day06().startPoint(grid)
+
+        // assert
+        assertThat(startPoint).isEqualTo(Point2D(4, 6))
+    }
+
+    @Test
+    fun should_return_false_when_position_is_not_blocked() {
+
+        // act
+        val blocked = Day06().isBlocked(Point2D(0, 0), grid)
+
+        // assert
+        assertThat(blocked).isFalse()
+    }
+
+    @Test
+    fun should_return_true_when_position_is_blocked() {
+
+        // act
+        val blocked = Day06().isBlocked(Point2D(4, 0), grid)
+
+        // assert
+        assertThat(blocked).isTrue()
+    }
+
+    @Test
+    fun should_move_when_direction_is_given() {
+
+        // act
+        val result = Day06().move(Point2D(4, 6), Point2D.NORTH, grid)
+
+        // assert
+        assertThat(result).isEqualTo(Point2D(4, 5))
+    }
+
+    @Test
+    fun should_return_END_POINT_when_moving_of_gird() {
+
+        // act
+        val result = Day06().move(Point2D(0, 0), Point2D.NORTH, grid)
+
+        // assert
+        assertThat(result).isEqualTo(Day06.END_POINT)
+
+    }
+
+    @Test
+    fun should_turn_east_when_direction_is_north() {
+
+        // act
+        val result = Day06().turn(Point2D.NORTH)
+
+        // assert
+        assertThat(result).isEqualTo(Point2D.EAST)
+    }
+
 }
