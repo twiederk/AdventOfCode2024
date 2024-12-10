029839d5bb624c9279f1278c92a2da05ac58a24f
IN57KC <torsten.wiederkehr@ing.com>
2024-12-10 17:38:04 +0100

    Restart of Day 09

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -10,23 +10,5 @@ class Day09 {
         return fileSystem.mapIndexed { index, char -> if (char == '.') 0 else index * char.digitToInt()  }.sum()
     }
 
-    fun createFileSystem(size: Int): String {
-        return ".".repeat(size)
-    }
-
-    fun createFileBlocks(fileSystem: String): List<FileBlock> {
-        return fileSystem.mapIndexed { index, char ->
-            if (index % 2 == 0) {
-                FileBlock(index, char, char.digitToInt())
-            } else {
-                FileBlock(index, '.', char.digitToInt())
-            }
-        }
-    }
 }
 
-data class FileBlock(
-    val id: Int,
-    val content: Char,
-    val size: Int
-)
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -6,14 +6,12 @@ class Day09Test {
 
     @Test
     fun should_load_data() {
-        // arrange
 
         // act
         val data = Day09().loadData(Path.of("src", "test", "resources", "Day09_TestData.txt"))
 
         // assert
         assertThat(data).isEqualTo("2333133121414131402")
-
     }
 
     @Test
@@ -26,62 +24,6 @@ class Day09Test {
         assertThat(checksum).isEqualTo(1928)
     }
 
-    @Test
-    fun should_create_empty_filesystem() {
-
-        // act
-        val fileSystem = Day09().createFileSystem(20_000)
-
-        // assert
-        assertThat(fileSystem.length).isEqualTo(20_000)
-    }
-
-    @Test
-    fun should_create_fileBlocks() {
-        // arrange
-        val fileSystem = "12345"
-
-        // act
-        val fileBlocks = Day09().createFileBlocks(fileSystem)
-
-        // assert
-        assertThat(fileBlocks).containsExactly(
-            FileBlock(0, '1', 1),
-            FileBlock(1, '.', 2),
-            FileBlock(2, '3', 3),
-            FileBlock(3, '.', 4),
-            FileBlock(4, '5', 5)
-        )
-    }
-
-    @Test
-    fun should_print_file_blocks() {
-        // arrange
-        val fileBlocks = listOf(
-            FileBlock(0, '1', 1),
-            FileBlock(1, '.', 2),
-            FileBlock(2, '3', 3),
-            FileBlock(3, '.', 4),
-            FileBlock(4, '5', 5)
-        )
-
-        // act
-        val result = Day09().printFileBlocks(fileBlocks)
-
-        // assert
-        assertThat(result).isEqualTo("0..111....22222")
-    }
 }
 
-private fun Day09.printFileBlocks(fileBlocks: List<FileBlock>): String {
-    var pointer = 0
-    val fileSystem = StringBuilder()
-    fileBlocks.forEach { fileBlock ->
-        fileSystem.append(".".repeat(fileBlock.size))
-        fileSystem.replace(pointer, pointer + 1, fileBlock.id.toString())
-        pointer += fileBlock.size
-    }
-
-    return String(fileSystem)
-}
 
