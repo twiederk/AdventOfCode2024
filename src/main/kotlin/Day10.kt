d03331c50cef3ff6ccdc2dcca248794998a605a5
IN57KC <torsten.wiederkehr@ing.com>
2024-12-10 11:25:12 +0100

    Checking all examples

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -18,4 +18,42 @@ class Day10 {
         return trailheads
     }
 
+    fun score(grid: List<String>, trailhead: Point2D): Int {
+        val targets = mutableSetOf<Point2D>()
+        val visited = mutableSetOf<Point2D>()
+        val queue = mutableListOf(trailhead)
+        while (queue.isNotEmpty()) {
+            val current = queue.removeAt(0)
+            if (height(current, grid) == 9) {
+                targets.add(current)
+                continue
+            }
+            if (visited.contains(current)) {
+                continue
+            }
+            visited.add(current)
+            val neighbours = current.cardinalNeighbors()
+            val currentHeight = height(current, grid)
+            neighbours.forEach { neighbour ->
+                if (isSafe(neighbour, currentHeight, grid)) {
+                    queue.add(neighbour)
+                }
+            }
+        }
+        return targets.size
+    }
+
+    fun isSafe(position: Point2D, currentHeight: Int, grid: List<String>): Boolean {
+        val xRange = 0..<grid[0].length
+        val yRange = 0..<grid.size
+        return position.x in xRange && position.y in yRange && currentHeight + 1 == height(position, grid)
+    }
+
+    fun height(position: Point2D, grid: List<String>): Int {
+        val char = grid[position.y][position.x]
+        return if (char == '.') -1 else {
+            char.digitToInt()
+        }
+    }
+
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -4,6 +4,44 @@ import java.nio.file.Path
 
 class Day10Test {
 
+    private val example_1 = listOf(
+        "0123",
+        "1234",
+        "8765",
+        "9876",
+    )
+
+    private val example_2 = listOf(
+        "...0...",
+        "...1...",
+        "...2...",
+        "6543456",
+        "7.....7",
+        "8.....8",
+        "9.....9"
+    )
+
+    private val example_3 = listOf(
+        "..90..9",
+        "...1.98",
+        "...2..7",
+        "6543456",
+        "765.987",
+        "876....",
+        "987....",
+    )
+
+    private val example_4 = listOf(
+        "10..9..",
+        "2...8..",
+        "3...7..",
+        "4567654",
+        "...8..3",
+        "...9..2",
+        ".....01",
+    )
+
+
     private val exampleData = listOf(
         "89010123",
         "78121874",
@@ -33,6 +71,98 @@ class Day10Test {
 
         // assert
         assertThat(trailheads).hasSize(9)
+    }
 
+    @Test
+    fun should_count_hiking_trails() {
+        // arrange
+        val trailhead = Point2D(4, 0)
+
+        // act
+        val hikingTrailsCount = Day10().score(exampleData, trailhead)
+
+        // assert
+        assertThat(hikingTrailsCount).isEqualTo(5)
+
+    }
+
+    @Test
+    fun should_return_char_from_grid_at_position() {
+        // act
+        val char = Day10().height(Point2D(0, 0), exampleData)
+
+        // assert
+        assertThat(char).isEqualTo(8)
+
+    }
+
+    @Test
+    fun should_check_if_point_is_safe() {
+        // arrange
+
+        // act
+        val result = Day10().isSafe(Point2D(0, 0), 7, exampleData)
+
+        // assert
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun should_return_false_for_point_outside_of_grid() {
+
+        // act
+        val result = Day10().isSafe(Point2D(4, -1), 0, exampleData)
+
+        // assert
+        assertThat(result).isFalse()
+
+    }
+
+    @Test
+    fun should_return_score_1_for_example_1() {
+        // arrange
+
+        // act
+        val score = Day10().score(example_1, Point2D(0, 0))
+
+        // assert
+        assertThat(score).isEqualTo(1)
+    }
+
+    @Test
+    fun should_return_minus_one_for_test_data() {
+
+        // act
+        val height = Day10().height(Point2D(0, 0), example_2)
+
+        // assert
+        assertThat(height).isEqualTo(-1)
+    }
+
+    @Test
+    fun should_return_score_2_for_example_2() {
+        // act
+        val height = Day10().score(example_2, Point2D(3, 0))
+
+        // assert
+        assertThat(height).isEqualTo(2)
+    }
+
+    @Test
+    fun should_return_score_4_for_example_3() {
+        // act
+        val height = Day10().score(example_3, Point2D(3, 0))
+
+        // assert
+        assertThat(height).isEqualTo(4)
+    }
+
+    @Test
+    fun should_return_score_1_for_example_4_with_trailhead_top() {
+        // act
+        val height = Day10().score(example_4, Point2D(1, 0))
+
+        // assert
+        assertThat(height).isEqualTo(1)
     }
 }
