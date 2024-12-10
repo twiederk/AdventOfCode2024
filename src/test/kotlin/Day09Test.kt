c1a6025138ccfbb7495d561c12098bd77d6e04d5
IN57KC <torsten.wiederkehr@ing.com>
2024-12-10 18:13:08 +0100

    Added test for given example

--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -35,6 +35,34 @@ class Day09Test {
         assertThat(filesystem).containsExactly(0, FREE, FREE, 1, 1, 1, FREE, FREE, FREE, FREE, 2, 2, 2, 2, 2)
     }
 
+    @Test
+    fun should_convert_disk_map_2333133121414131402_to_filesystem() {
+
+        // act
+        val filesystem = Day09().filesystem("2333133121414131402")
+
+        // assert
+        assertThat(filesystem).containsExactly(
+            0, 0,
+            FREE, FREE, FREE,
+            1, 1, 1,
+            FREE, FREE, FREE,
+            2,
+            FREE, FREE, FREE,
+            3, 3, 3,
+            FREE,
+            4, 4,
+            FREE,
+            5, 5, 5, 5,
+            FREE,
+            6, 6, 6, 6,
+            FREE,
+            7, 7, 7,
+            FREE,
+            8, 8, 8, 8,
+            9, 9
+        )
+    }
 }
 
 
