b1078389cbc25eef2684f47eef54769c39e5377f
IN57KC <torsten.wiederkehr@ing.com>
2024-12-10 09:35:05 +0100

    Started with Day09, but still to sick

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,6 +1,32 @@
+import java.nio.file.Path
+
 class Day09 {
 
     fun checksum(fileSystem: String): Int {
         return fileSystem.mapIndexed { index, char -> if (char == '.') 0 else index * char.digitToInt()  }.sum()
     }
+
+    fun createFileSystem(size: Int): String {
+        return ".".repeat(size)
+    }
+
+    fun loadData(path: Path): String {
+        return Resources.resourceAsText(path.fileName.toString())
+    }
+
+    fun createFileBlocks(fileSystem: String): List<FileBlock> {
+        return fileSystem.mapIndexed { index, char ->
+            if (index % 2 == 0) {
+                FileBlock(index, char, char.digitToInt())
+            } else {
+                FileBlock(index, '.', char.digitToInt())
+            }
+        }
+    }
 }
+
+data class FileBlock(
+    val id: Int,
+    val content: Char,
+    val size: Int
+)
