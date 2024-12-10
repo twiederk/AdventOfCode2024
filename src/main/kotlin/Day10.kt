915ee2aafa2962b4fb1cd808fbb7de149e3b68b7
IN57KC <torsten.wiederkehr@ing.com>
2024-12-10 09:45:37 +0100

    Find trailheads.

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -6,4 +6,16 @@ class Day10 {
         return Resources.resourceAsListOfString(path.fileName.toString())
     }
 
+    fun trailheads(grid: List<String>): List<Point2D> {
+        val trailheads = mutableListOf<Point2D>()
+        grid.forEachIndexed { y, row ->
+            row.forEachIndexed { x, cell ->
+                if (cell == '0') {
+                    trailheads.add(Point2D(x, y))
+                }
+            }
+        }
+        return trailheads
+    }
+
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -4,6 +4,17 @@ import java.nio.file.Path
 
 class Day10Test {
 
+    private val exampleData = listOf(
+        "89010123",
+        "78121874",
+        "87430965",
+        "96549874",
+        "45678903",
+        "32019012",
+        "01329801",
+        "10456732",
+    )
+
     @Test
     fun should_load_data() {
         // act
@@ -13,4 +24,15 @@ class Day10Test {
         assertThat(grid).hasSize(8)
     }
 
+    @Test
+    fun should_find_all_trailheads() {
+        // arrange
+
+        // act
+        val trailheads = Day10().trailheads(exampleData)
+
+        // assert
+        assertThat(trailheads).hasSize(9)
+
+    }
 }
