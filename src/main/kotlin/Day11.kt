4b0074588b1b6c9104f9c372c3b10927555c3cc8
IN57KC <torsten.wiederkehr@ing.com>
2024-12-11 06:42:31 +0100

    Implemented Rule1 and Rule2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -6,4 +6,34 @@ class Day11 {
         val data = Resources.resourceAsText(path.fileName.toString())
         return data.split(' ').map { it.toLong() }
     }
+
+    interface Rule {
+        fun usable(stone: Long): Boolean
+        fun execute(stone: Long): List<Long>
+    }
+
+    class Rule1 : Rule {
+
+        override fun usable(stone: Long): Boolean {
+            return stone == 0L
+        }
+
+        override fun execute(stone: Long): List<Long> {
+            return listOf(1L)
+        }
+    }
+
+    class Rule2 : Rule {
+
+        override fun usable(stone: Long): Boolean {
+            return stone.toString().length % 2 == 0
+        }
+
+        override fun execute(stone: Long): List<Long> {
+            val half = stone.toString().length / 2
+            val firstHalf = stone.toString().substring(0, half)
+            val secondHalf = stone.toString().substring(half)
+            return listOf(firstHalf.toLong(), secondHalf.toLong())
+        }
+    }
 }
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -14,4 +14,60 @@ class Day11Test {
         assertThat(data).containsExactly(125L, 17L)
     }
 
+    @Test
+    fun should_return_true_when_Rule1_is_usable() {
+        // act
+        val result = Day11.Rule1().usable(0L)
+
+        // assert
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun should_return_false_when_Rule1_is_not_usable() {
+        // act
+        val result = Day11.Rule1().usable(1L)
+
+        // assert
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun should_execute_Rule1() {
+
+        // act
+        val stones = Day11.Rule1().execute(0L)
+
+        // assert
+        assertThat(stones).containsExactly(1L)
+    }
+
+    @Test
+    fun should_return_true_when_Rule2_is_usable() {
+        // act
+        val result = Day11.Rule2().usable(1000L)
+
+        // assert
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun should_return_false_when_Rule2_is_not_usable() {
+        // act
+        val result = Day11.Rule2().usable(100L)
+
+        // assert
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun should_execute_Rule2() {
+
+        // act
+        val stones = Day11.Rule2().execute(1000L)
+
+        // assert
+        assertThat(stones).containsExactly(10L, 0L)
+    }
+
 }
