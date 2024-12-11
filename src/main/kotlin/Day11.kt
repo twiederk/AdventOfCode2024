0fea3590411f4ecbac2e7d32beb8c63b16103e5d
IN57KC <torsten.wiederkehr@ing.com>
2024-12-11 06:24:05 +0100

    Implemented loadData

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,2 +1,9 @@
+import java.nio.file.Path
+
 class Day11 {
+
+    fun loadData(path: Path): List<Long> {
+        val data = Resources.resourceAsText(path.fileName.toString())
+        return data.split(' ').map { it.toLong() }
+    }
 }
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Path
+
 class Day11Test {
+
+    @Test
+    fun should_load_data() {
+
+        // act
+        val data = Day11().loadData(Path.of("src", "test", "resources", "Day11_TestData.txt"))
+
+        // assert
+        assertThat(data).containsExactly(125L, 17L)
+    }
+
 }
