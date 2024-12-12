5544fa0982566e37e66e74bb3240633c0d364772
IN57KC <torsten.wiederkehr@ing.com>
2024-12-12 08:00:23 +0100

    Implemented area()

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -5,4 +5,36 @@ class Day12 {
     fun loadData(path: Path): List<String> {
         return Resources.resourceAsListOfString(path.fileName.toString())
     }
+
+    fun plant(garden: List<String>, plot: Point2D): Char {
+        return garden[plot.y][plot.x]
+    }
+
+    fun area(garden: List<String>, start: Point2D): List<Point2D> {
+        val area = mutableListOf<Point2D>()
+        val visited = mutableSetOf<Point2D>()
+        val queue = mutableListOf<Point2D>()
+
+        queue.add(start)
+        val plant = plant(garden, start)
+
+        while (queue.isNotEmpty()) {
+            val current = queue.removeAt(0)
+            if (visited.contains(current)) {
+                continue
+            }
+            visited.add(current)
+            area.add(current)
+
+            val neighbors = current.cardinalNeighbors().filter { isSafe(garden, it, plant) }
+            queue.addAll(neighbors)
+        }
+        return area
+    }
+
+    fun isSafe(garden: List<String>, plot: Point2D, plant: Char): Boolean {
+        return plot.x >= 0 && plot.y >= 0 && plot.x < garden.size && plot.y < garden[0].length
+                && plant == plant(garden, plot)
+    }
+
 }
--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -4,6 +4,13 @@ import java.nio.file.Path
 
 class Day12Test {
 
+    private val garden1 = listOf(
+        "AAAA",
+        "BBCD",
+        "BBCC",
+        "EEEC",
+    )
+
     @Test
     fun should_load_data() {
 
@@ -14,4 +21,27 @@ class Day12Test {
         assertThat(garden).hasSize(10)
     }
 
+    // find all areas using bfs
+    // go through each plot and check if its part of an area
+    // if no, find area using bfs
+    // if yes, continue with next plot
+
+    // count fences by checking each side of each element of an area
+
+    @Test
+    fun should_find_area() {
+        // arrange
+
+        // act
+        val area = Day12().area(garden1, Point2D(0, 0))
+
+        // assert
+        assertThat(area).containsOnly(
+            Point2D(0, 0),
+            Point2D(1, 0),
+            Point2D(2, 0),
+            Point2D(3, 0),
+        )
+
+    }
 }
